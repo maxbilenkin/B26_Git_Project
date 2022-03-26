941c99b4bf4baf797d103fc09903fc18bf880037
Maxim <maxbilenkin@gmail.com>
2022-03-26 16:11:40 -0400

    Updated First File

--- src/day0/First.java
+++ src/day0/First.java
@@ -1,9 +1,12 @@
 package day0;
 
 public class First {
+    public static void main(String[] args) {
 
-    //TODO: Works on this tomorrow
-    // add some cool code
+        System.out.println("java");
+        System.out.println("Working...");
+    }
+
 
     // extra line 1
     // extra line 2
